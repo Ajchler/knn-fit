8214d0857df96c89fb86d9b9b7e3a07f86f0a063
Vojtech Eichler <vojta.eich@gmail.com>
2024-03-03 20:52:53 +0100

    Add AnnotatorAPI class provided by Hradis

---
+++ annotator_api.py
@@ -0,0 +1,35 @@
+import requests
+from contextlib import contextmanager
+import logging
+
+class Annotator_API():
+    def __init__(self, base_url: str, login: str, password: str):
+        self.base_url = base_url
+        self.login = login
+        self.password = password
+        self.session = None
+
+    def login_token(self):
+        login_request = self.session.post(self.base_url + '/api/token', data={'username': self.login, 'password': self.password})
+        if login_request.status_code != 200:
+            logging.error(f'Failed to login: {login_request.status_code} {login_request.text}')
+            raise Exception(f'Failed to login: {login_request.status_code} {login_request.text}')
+
+    @contextmanager
+    def API_session(self):
+        try:
+            self.session = requests.Session()
+            self.login_token()
+            yield self
+        finally:
+            if self.session:
+                self.session.close()
+
+    def get(self, url: str):
+        response = self.session.get(url)
+        if response.status_code == 401:
+            self.session.post(self.base_url + '/api/token/renew')
+            if response.status_code != 200:
+                raise Exception(f'Failed to renew token: {response.status_code} {response.text}')
+            response = self.session.get(url)
+        return response
